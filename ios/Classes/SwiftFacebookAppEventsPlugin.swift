e89052ad1f6c8ea25895ec37b7388b18814a8bd4
misa <misael.riojas@fulltimeforce.com>
2022-03-23 19:59:22 +0100

    init

---
+++ ios/Classes/SwiftFacebookAppEventsPlugin.swift
@@ -0,0 +1,174 @@
+import Flutter
+import UIKit
+import FBSDKCoreKit
+import FBSDKCoreKit_Basics
+import FBAudienceNetwork
+
+public class SwiftFacebookAppEventsPlugin: NSObject, FlutterPlugin {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter.oddbit.id/facebook_app_events", binaryMessenger: registrar.messenger())
+        let instance = SwiftFacebookAppEventsPlugin()
+
+        // Required for FB SDK 9.0, as it does not initialize the SDK automatically any more.
+        // See: https://developers.facebook.com/blog/post/2021/01/19/introducing-facebook-platform-sdk-version-9/
+        // "Removal of Auto Initialization of SDK" section
+        ApplicationDelegate.shared.initializeSDK()
+
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "clearUserData":
+            handleClearUserData(call, result: result)
+            break
+        case "setUserData":
+            handleSetUserData(call, result: result)
+            break
+        case "clearUserID":
+            handleClearUserID(call, result: result)
+            break
+        case "flush":
+            handleFlush(call, result: result)
+            break
+        case "getApplicationId":
+            handleGetApplicationId(call, result: result)
+            break
+        case "logEvent":
+            handleLogEvent(call, result: result)
+            break
+        case "logPushNotificationOpen":
+            handlePushNotificationOpen(call, result: result)
+            break
+        case "setUserID":
+            handleSetUserId(call, result: result)
+            break
+        case "setAutoLogAppEventsEnabled":
+            handleSetAutoLogAppEventsEnabled(call, result: result)
+            break
+        case "setDataProcessingOptions":
+            handleSetDataProcessingOptions(call, result: result)
+            break
+        case "logPurchase":
+            handlePurchased(call, result: result)
+            break
+        case "getAnonymousId":
+            handleHandleGetAnonymousId(call, result: result)
+            break
+        case "setAdvertiserTracking":
+            handleSetAdvertiserTracking(call, result: result)
+            break
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func handleClearUserData(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        AppEvents.clearUserData()
+        result(nil)
+    }
+
+    private func handleSetUserData(_ call: FlutterMethodCall, result: @escaping FlutterResult) {        
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+
+        AppEvents.setUserData(arguments["email"] as? String, forType: FBSDKAppEventUserDataType.email)
+        AppEvents.setUserData(arguments["firstName"] as? String, forType: FBSDKAppEventUserDataType.firstName)
+        AppEvents.setUserData(arguments["lastName"] as? String, forType: FBSDKAppEventUserDataType.lastName)
+        AppEvents.setUserData(arguments["phone"] as? String, forType: FBSDKAppEventUserDataType.phone)
+        AppEvents.setUserData(arguments["dateOfBirth"] as? String, forType: FBSDKAppEventUserDataType.dateOfBirth)
+        AppEvents.setUserData(arguments["gender"] as? String, forType: FBSDKAppEventUserDataType.gender)
+        AppEvents.setUserData(arguments["city"] as? String, forType: FBSDKAppEventUserDataType.city)
+        AppEvents.setUserData(arguments["state"] as? String, forType: FBSDKAppEventUserDataType.state)
+        AppEvents.setUserData(arguments["zip"] as? String, forType: FBSDKAppEventUserDataType.zip)
+        AppEvents.setUserData(arguments["country"] as? String, forType: FBSDKAppEventUserDataType.country)
+
+        result(nil)
+    }
+
+    private func handleClearUserID(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        AppEvents.clearUserID()
+        result(nil)
+    }
+
+    private func handleFlush(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        AppEvents.flush()
+        result(nil)
+    }
+
+    private func handleGetApplicationId(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(Settings.appID)
+    }
+
+    private func handleHandleGetAnonymousId(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(AppEvents.anonymousID)
+    }
+
+    private func handleLogEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let eventName = arguments["name"] as! String
+        let parameters = arguments["parameters"] as? [AppEvents.ParameterName: Any] ?? [AppEvents.ParameterName: Any]()
+        if arguments["_valueToSum"] != nil && !(arguments["_valueToSum"] is NSNull) {
+            let valueToDouble = arguments["_valueToSum"] as! Double
+            AppEvents.logEvent(AppEvents.Name(eventName), valueToSum: valueToDouble, parameters: parameters)
+        } else {
+            AppEvents.logEvent(AppEvents.Name(eventName), parameters: parameters)
+        }
+
+        result(nil)
+    }
+
+    private func handlePushNotificationOpen(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let payload = arguments["payload"] as? [String: Any]
+        if let action = arguments["action"] {
+            let actionString = action as! String
+            AppEvents.logPushNotificationOpen(payload!, action: actionString)
+        } else {
+            AppEvents.logPushNotificationOpen(payload!)
+        }
+
+        result(nil)
+    }
+
+    private func handleSetUserId(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let id = call.arguments as! String
+        AppEvents.userID = id
+        result(nil)
+    }
+
+    private func handleSetAutoLogAppEventsEnabled(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let enabled = call.arguments as! Bool
+        Settings.isAutoLogAppEventsEnabled = enabled
+        result(nil)
+    }
+
+    private func handleSetDataProcessingOptions(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let modes = arguments["options"] as? [String] ?? []
+        let state = arguments["state"] as? Int32 ?? 0
+        let country = arguments["country"] as? Int32 ?? 0
+
+        Settings.setDataProcessingOptions(modes, country: country, state: state)
+
+        result(nil)
+    }
+
+    private func handlePurchased(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let amount = arguments["amount"] as! Double
+        let currency = arguments["currency"] as! String
+        let parameters = arguments["parameters"] as? [String: Any] ?? [String: Any]()
+        AppEvents.logPurchase(amount, currency: currency, parameters: parameters)
+
+        result(nil)
+    }
+
+    private func handleSetAdvertiserTracking(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let enabled = arguments["enabled"] as! Bool
+        let collectId = arguments["collectId"] as! Bool
+        Settings.setAdvertiserTrackingEnabled(enabled)
+        Settings.isAdvertiserIDCollectionEnabled = collectId
+        result(nil)
+    }
+}
